03d1466de158d98c3d689c0fbb3b6fb9e0a4341b
Leonardo Adams <leonardoadams@gmail.com>
2019-09-06 15:51:49 -0300

    Add Entry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import { getMetricDataInfo } from '../utils/helpers'
+import { View, Text, TouchableOpacity, Platform, StyleSheet } from 'react-native'
+import { 
+    getMetricDataInfo, 
+    timeToString, 
+    getDailyReminderValue 
+} from '../utils/helpers'
+import FitnessSlider from './FitnessSlider'
+import FitnessStepper from './FitnessStepper'
+import DateHeader from './DateHeader'
+import { Ionicons } from '@expo/vector-icons'
+import TextButton from './TextButton'
+import { submitEntry, removeEntry } from '../utils/api'
+import { connect } from 'react-redux'
+import { addEntry } from '../actions'
+import { white, purple } from '../utils/colors'
 
-export default class AddEntry extends Component {
+function SubmitBtn ({ onPress }) {
+    return (
+        <TouchableOpacity
+            style={ Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn } 
+            onPress={onPress}
+        >
+            <Text style={styles.submitBtnText}>SUBMIT</Text>
+        </TouchableOpacity>
+    )
+}
+
+class AddEntry extends Component {
     state = {
         run: 0,
         bike: 0,
@@ -14,7 +38,7 @@ export default class AddEntry extends Component {
         const { max, step } = getMetricDataInfo(metric)
 
         this.setState((currState) => {
-            const count = currState[metric] - step
+            const count = currState[metric] + step
 
             return {
                 ...currState,
@@ -23,6 +47,7 @@ export default class AddEntry extends Component {
         })
     }
     decrement = (metric) => {
+        const { step } = getMetricDataInfo(metric)
         this.setState((currState) => {
             const count = currState[metric] - step
 
@@ -37,11 +62,133 @@ export default class AddEntry extends Component {
             [metric]: value
         }))
     }
+    submit = () => {
+        const key = timeToString()
+        const entry = this.state
+
+        this.props.dispatch(addEntry({
+            [key]: entry
+        }))
+
+        submitEntry({ key, entry })
+
+        this.setState(() => ({
+            run: 0,
+            bike: 0,
+            swim: 0,
+            sleep: 0,
+            eat: 0,            
+        }))
+    }
+    reset = () => {
+        const key = timeToString()
+
+        this.props.dispatch(addEntry({
+            [key]: getDailyReminderValue()
+        }))
+
+        removeEntry(key)
+    }
     render() {
+        const metaInfo = getMetricDataInfo()
+
+        if (this.props.alreadyLogged) {
+            return (
+                <View style={styles.center}>
+                    <Ionicons 
+                        name={Platform.OS === 'ios' ? 'ios-happy' : 'md-happy'} 
+                        size={188} 
+                    />
+                    <Text>You already logged your information for today</Text>
+                    <TextButton style={{ padding: 10 }} onPress={this.reset}>
+                        Reset
+                    </TextButton>
+                </View>
+            )
+        }
+
         return (
-            <View>
-                {getMetricDataInfo('bike').getIcon()}
+            <View style={styles.container}>
+                <DateHeader date={(new Date()).toLocaleDateString()} />
+                {Object.keys(metaInfo).map((key) => {
+                    const { getIcon, type, ...rest } = metaInfo[key]
+                    const value = this.state[key]
+
+                    return (
+                        <View key={key} style={styles.row}>
+                            {getIcon()}
+                            {type === 'slider'
+                                ? <FitnessSlider 
+                                    value={value}
+                                    onChange={(value) => this.slide(key, value)}
+                                    {...rest}
+                                  />
+                                : <FitnessStepper
+                                    value={value}
+                                    onIncrement={() => this.increment(key)}
+                                    onDecrement={() => this.decrement(key)}
+                                    {...rest}
+                                  />
+                            }
+                        </View>
+                    )
+                })}
+                <SubmitBtn onPress={this.submit} />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: white,
+    },
+    row: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center'
+    },
+    iosSubmitBtn: {
+        backgroundColor: purple,
+        padding: 10,
+        borderRadius: 7,
+        height: 45,
+        marginLeft: 40,
+        marginRight: 40,
+    },
+    androidSubmitBtn: {
+        backgroundColor: purple,
+        padding: 10,
+        paddingLeft: 30,
+        paddingRight: 30,
+        height: 45,
+        borderRadius: 2,
+        alignSelf: 'flex-end',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    submitBtnText: {
+        color: white,
+        fontSize: 22,
+        textAlign: 'center',
+    },
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 30,
+        marginRight: 30,
+    }
+})
+
+function mapStateToProps (state) {
+    const key = timeToString()
+
+    return {
+        alreadyLogged: state[key] && typeof state[key].today === 'undefined'
+    }
+}
+
+export default connect(mapStateToProps)(AddEntry)
