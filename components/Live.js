9341f98252b910dce7f5176e26dd71e245e4a137
Leonardo Adams <leonardoadams@gmail.com>
2019-09-12 10:33:21 -0300

    Live component - undetermined UI

---
+++ components/Live.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import { View, Text, ActivityIndicator, StyleSheet, TouchableOpacity } from 'react-native'
+import { Foundation } from '@expo/vector-icons'
+import { purple, white } from '../utils/colors'
+
+
+export default class Live extends Component {
+    state = {
+        coords: null,
+        status: 'undetermined',
+        direction: ''
+    }
+    askPermission = () => {
+        
+    }
+    render() {
+        const { coords, status, direction } = this.state
+
+        if (status === null) {
+            return <ActivityIndicator style={{ marginTop: 30 }} />
+        }
+
+        if (status === 'denied') {
+            return (
+                <View>
+                    <Text>Denied</Text>
+                </View>
+            )
+        }
+
+        if (status === 'undetermined') {
+            return (
+                <View style={styles.center}>
+                    <Foundation name='alert' size={30} />
+                    <Text>
+                        You need to enable location services for this app.
+                    </Text>
+                    <TouchableOpacity onPress={this.askPermission} style={styles.button}>
+                        <Text style={styles.buttonText}>
+                            Enable
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            )
+        }
+
+        return (
+            <View>
+                <Text>Live</Text>    
+                <Text>{JSON.stringify(this.state)}</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between'
+    },
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 30,
+        marginRight: 30,
+    },
+    button: {
+        padding: 10,
+        backgroundColor: purple,
+        alignSelf: 'center',
+        borderRadius: 5,
+        margin: 20
+    },
+    buttonText: {
+        color: white,
+        fontSize: 20,
+    }
+})
